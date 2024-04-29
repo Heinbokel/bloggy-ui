bba9d1cdd5295ba5b87808729c50e28419f3d5a9
Robert Heinbokel <robertheinbokel@gmail.com>
2024-04-29 08:05:24 -0400

    more custom error messages, etc.

--- Models/UserRegisterRequest.cs
+++ Models/UserRegisterRequest.cs
@@ -7,18 +7,18 @@ namespace bloggy_ui.Models;
 /// </summary>
 public class UserRegisterRequest {
 
-    [Required]
-    [MinLength(2)]
-    [MaxLength(64)]
+    [Required(ErrorMessage = "Username must be provided.")]
+    [MinLength(2, ErrorMessage = "Username must be at least 2 characters.")]
+    [MaxLength(64, ErrorMessage = "Username must be 64 characters or less.")]
     public string UserName { get; set;}
 
     [Required]
-    [EmailAddress]
+    [EmailAddress(ErrorMessage = "Email must be a valid email address.")]
     public string Email { get; set;}
 
     [Required]
-    [MinLength(8)]
-    [MaxLength(256)]
+    [MinLength(8, ErrorMessage = "Password must be at least 8 characters.")]
+    [MaxLength(256, ErrorMessage = "pASSWORD must be at 256 characters or less.")]
     public string Password { get; set;}
 
     [Required]
@@ -35,6 +35,7 @@ public class UserRegisterRequest {
     public DateOnly DateOfBirth { get; set;}
 
     [Required]
+    [Range(1, 2, ErrorMessage = "User role ID must be 1 or 2.")]
     public int UserRoleId { get; set;}
 
 }
